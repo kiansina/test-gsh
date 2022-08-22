0e4af9ca49ffc0e0061d2c2a7b7ee5cd481bfef4
Sina Kian <70090343+kiansina@users.noreply.github.com>
2022-08-22 15:09:27 +0200

    Update gsh.py

--- gsh.py
+++ gsh.py
@@ -1,31 +1,21 @@
 # streamlit_app.py
-
 import streamlit as st
+import pandas as pd
 from google.oauth2 import service_account
-from gsheetsdb import connect
-
-
-
+from gspread_pandas import Spread,Client
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
-    scopes=[
+    scopes=["https://www.googleapis.com/auth/drive",
         "https://www.googleapis.com/auth/spreadsheets",
     ],
 )
-conn = connect(credentials=credentials)
+client=Client(scope=["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/spreadsheets"], creds=credentials)
+spreadsheetname="test"
+spread=Spread(spreadsheetname,client=client)
 
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
 
-sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+sh=client.open(spreadsheetname)
+worksheet_list = sh.worksheets()
 
-# Print results.
-for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
+st.write(spread.url)
