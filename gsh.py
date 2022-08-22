5a9fef07834c39c4727c39cc9cc30863c31334ab
Sina Kian <70090343+kiansina@users.noreply.github.com>
2022-08-22 13:00:32 +0200

    Update gsh.py

--- gsh.py
+++ gsh.py
@@ -3,8 +3,8 @@
 import streamlit as st
 from google.oauth2 import service_account
 from gsheetsdb import connect
-from collections.abc import Iterable
-collections.Iterable = collections.abc.Iterable
+
+
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
